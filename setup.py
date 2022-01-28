a3e5b44ac6d567dcc573ae1f1f798f5b02820a33
Vi <rosascript@gmail.com>
2022-01-27 21:05:31 -0500

    balls
    balls
    balls

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ def setup():
   print('Python 4.1 installer.')
   print('Currently installing balls')
   print('Could not find balls.')
+  print('skill issue cus your mom = dead')
+  print('btw your gay: https://www.youtube.com/watch?v=0V_vn5HbqGw')
   # lmao
 
 if __name__ == '__main__':
